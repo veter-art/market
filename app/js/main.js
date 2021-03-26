91b7474dc120554966f6cf5866be58a5dbbc7954
veter-art <vveetteer@yandex.ru>
2021-03-26 21:33:25 +0300

    finish product

--- app/js/main.js
+++ app/js/main.js
@@ -4,10 +4,37 @@ $(function(){
 
 
     $(".rate-star").rateYo({
-      rating: 4.6,
+      rating: 3.6,
+      starWidth: "15px",
+      readOnly: true,
+      ratedFill: "#ffc000",
+      normalFill: "#4a3e1a",
+      
+     });
+     $(".product__rate-star").rateYo({
+      rating: 3.6,
       starWidth: "12px",
       readOnly: true,
+      ratedFill: "#ffc000",
+      normalFill: "#4a3e1a",
+      
      });
+
+     var mixer = mixitup('.product__inner-box');
+
+     
+    //  $(".product__rate-star").rateYo({
+ 
+    //   "rating" : 3.2,
+    //   "starSvg": "<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">"+
+    //                "<path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379"+
+    //                          " 4.246-3.611-2.625-3.612 2.625"+
+    //                          " 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833"+
+    //                          " 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388"+
+    //                          " 7.416 5.388-2.833-8.718"+
+    //                          " 7.417-5.389h-9.167l-2.833-8.718z"></path>"+
+    //               "</svg>"
+    // });
 
 
 });
